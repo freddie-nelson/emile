99d646cefd7f3a875216d751c3d8916e39cb7747
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-10-15 21:34:32 +0100

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-7xl font-semibold text-blue-600">Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
